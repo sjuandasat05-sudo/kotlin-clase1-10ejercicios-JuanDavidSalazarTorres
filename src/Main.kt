bf51045bf0e22660ec05c9150fd168fb4f7ff344
Juan David Salazar T <sjuandasat05@gmail.com>
2026-07-26 18:29:04 -0500

    Se realizo el primer y segundo ejercicio

---
+++ src/Main.kt
@@ -0,0 +1,45 @@
+fun main() {
+    println("===EJERCICIO CLASE 1===")
+
+// ============== EJERCICIO 1 ==============//
+
+
+    println("---- Ejercicio1------")
+
+    println("Saludo personalizado")
+
+    print("Ingrese su nombre: ")
+    val nombre = readln()
+
+    print("Ingrese su edad: ")
+    val edad = readln().toInt()
+
+    println("Hola $nombre")
+    println("Tienes $edad años.")
+
+// ============ EJERCICIO 2 ============//
+
+    println("-----Ejercicio2--------")
+
+    println("Calculadora basica")
+
+    println("Escribe el primer numero")
+    val numero_1 = readln().toInt()
+
+    println("Escribe el segundo numero")
+    val numero_2 = readln().toInt()
+
+    println("Multiplicacion: ${numero_1 * numero_2}")
+
+    println("Division: ${numero_1 / numero_2}")
+
+    
+
+
+
+
+
+
+
+
+}
